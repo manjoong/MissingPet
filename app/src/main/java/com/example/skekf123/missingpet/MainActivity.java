0978ff16b83dd2674985c5abd9e1d678df35344e
KimManJoong <skekf123@naver,com>
2017-11-26 18:29:36 +0900

    detail view 디자인

--- app/src/main/java/com/example/skekf123/missingpet/MainActivity.java
+++ app/src/main/java/com/example/skekf123/missingpet/MainActivity.java
@@ -1,13 +1,57 @@
 package com.example.skekf123.missingpet;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar; //툴바 생성
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import static com.example.skekf123.missingpet.R.layout.support_simple_spinner_dropdown_item;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar; //툴바 선언
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.toolbar); //툴바설정
+        toolbar.setTitleTextColor(Color.parseColor("#ffffff"));
+        setSupportActionBar(toolbar);//액션바와 같게 만들어줌
+        // ↓툴바에 홈버튼을 활성화
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        // ↓툴바의 홈버튼의 이미지를 변경(기본 이미지는 뒤로가기 화살표)
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_action_back);
+
+    }
+
+    //추가된 소스, ToolBar에 menu.xml을 인플레이트함
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //return super.onCreateOptionsMenu(menu);
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+    //추가된 소스, ToolBar에 추가된 항목의 select 이벤트를 처리하는 함수
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //return super.onOptionsItemSelected(item);
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // User chose the "Settings" item, show the app settings UI...
+                Toast.makeText(getApplicationContext(), "환경설정 버튼 클릭됨", Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                Toast.makeText(getApplicationContext(), "나머지 버튼 클릭됨", Toast.LENGTH_LONG).show();
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
